32b414ec4bd9bda6b1531b8dae2d2a4cb3479ce1
F28 <f28.public@yahoo.com>
2025-09-14 19:11:41 +0700

    Add files via upload
    3. code_3 = Penjelasan '#include', 'int main','{} kurung kurawal', dan '; titik koma'

---
+++ code_3/code_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+//include -> sebagai pemanggil "library"
+//stdio.h -> nama library yang dipanggil untuk digunakan, karena commnad "printf" ada didalam library ini
+
+int main()
+//int main -> sebagai fungsi utama yang akan dieksekusi pc.(nama "main" bisa diganti)
+{ 
+//kurung kurawal -> menandakan bahwa fungsi "int main" diatas dari " {, buka kurawal" ke "}, tutup kurawal".
+	printf("Penjelasan '#include', 'int main','{} kurung kurawal', dan '; titik koma"); 
+	//titik koma -> selalu ada sebagai mengakhiri baris perintah.
+	
+	return 0; 
+}
