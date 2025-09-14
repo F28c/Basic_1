2836180995b64537ab3925707603df04a01b4738
F28 <f28.public@yahoo.com>
2025-09-14 19:13:38 +0700

    Add files via upload
    4. code_4 = Penjelasan 'printf', 'penggunaan \n',' penggunaan \t', 'tanda //' dan 'tanda /* sampai */'

---
+++ code_4/code_4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	printf("Selamat-selamat hari-hari biasanya--------\nsenang aja dan sedih aja.......\n");
+	//printf -> fungsi untuk menampilkan text ke layar "cmd/terminal"
+	//\n -> simplenya sebagai "ENTER" untuk tempat kata baru, kalo ga ada "kata" bakal nyambung di belakang
+	
+	//contoh tanpa "/n" :
+	printf("hari senang....");
+	printf("hari buruk----");
+	printf("hari ok++++ \n");
+	
+	printf("\tini kaya tab di word\tterlihat jarak tabnya\tsupaya enak kebaca");
+	//\t -> simplenya memberikan "tab" di awal kata seperti di "word"
+	
+	// tanda "//" -> namanya "comment" membuat kata dalam 1 baris tidak bisa dibaca program
+	// tanda "/* sampai */ membuat kata yang ada didalam tanda itu tidak bisa dibaca, tidak peduli sampai berapa baris.
+	
+	/*
+	ga bisa dibaca program
+	ga akan tampil
+	ga ada di layar
+	cuma buat pengingat
+	atau untuk menjelaskan program
+	*/
+	
+	return 0;
+}
